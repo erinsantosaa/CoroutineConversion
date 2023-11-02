bf184aadb991b8b3350ceb09262430354ae252ec
Erin Santosa <tun35949@temple.edu>
2023-11-02 11:48:23 -0400

    moved button & launch scope with handler activity repeating 100 times & delaying 40 ms

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.ImageView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,13 +32,15 @@ class MainActivity : AppCompatActivity() {
 
         val scope = CoroutineScope(Job() + Dispatchers.Default)
 
-//        findViewById<Button>(R.id.revealButton).setOnClickListener{
-//            Thread{
-//                repeat(100) {
-//                    handler.sendEmptyMessage(it)
-//                    Thread.sleep(40)
-//                }
-//            }.start()
-//        }
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            scope.launch {
+                repeat(100){
+                    withContext(Dispatchers.Default){
+                        handler.sendEmptyMessage(it)
+                    }
+                    delay(40)
+                }
+            }
+        }
     }
 }
